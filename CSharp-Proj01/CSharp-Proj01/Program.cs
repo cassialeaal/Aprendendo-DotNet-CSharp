fe50da9f7cfc14a8ece77b8b723667c7810a6931
cassialeaal <cassialeaal@gmail.com>
2022-02-07 22:26:42 -0300

    Aula sobre estrutura Namespace

---
+++ CSharp-Proj01/CSharp-Proj01/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    
+//Caso o using System não seja declarado no inicio do código
+//e queremos usar o Console, podemos declarar o System no namespace em questão.
+//Abaixo temos comentando o exemplo de como declará-lo neste caso.
+
+    //System.Console
+
+//Se o System já foi declarado no início do código, apenas adicionar o comando abaixo
+
+    //Console
+    class Namespace
+}
+
+//O namespace serve para separar as classes, e não é possível ter duas classes iguais
+//dentro de um único namespace
+//A estrutura abaixo não é válida e dará um erro pois a Classe já existe anteriormente:
+//namespace Primeiro
+//{
+//  class Classe
+//  {
+//
+//  }
+//
+//  class Classe
+//  {
+//
+//  }
+//}
