2574c18835a3666d97217eb5d4481cbd5093cba8
Cassia R M Leal <85911861+cassialeaal@users.noreply.github.com>
2022-02-11 15:03:02 -0300

    aplicacao console dotnet

--- Dotnet/Program.cs
+++ Dotnet/Program.cs
@@ -5,5 +5,5 @@ int numeroDeVezes = 10;
 
 for (int i = 0; i < numeroDeVezes; i++)
 {
-    Console.WriteLine($"Bem vindo ao curso de .NET {i}").
+    Console.WriteLine($"Bem vindo ao curso de .NET {i}");
 }
