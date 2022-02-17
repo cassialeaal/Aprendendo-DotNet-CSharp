646d702521de4d4884494ab2cf8d9e704eb6aed2
cassialeaal <cassialeaal@gmail.com>
2022-02-17 17:37:11 -0300

    Construtores e construtores padrão

---
+++ POO/ProjConstrutores/ExemploConstrutores/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa("Cassia", "Leal");
+            p1.Apresentar();
+            p2.Apresentar();
+        }
+    }
+}
