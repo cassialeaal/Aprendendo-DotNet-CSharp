f0e283bc30564e8b6555214525ab3f31b0337f32
cassialeaal <cassialeaal@gmail.com>
2022-02-21 16:33:54 -0300

    entendendo arrays

--- ExemploColecoes/Colecoes/Program.cs
+++ ExemploColecoes/Colecoes/Program.cs
@@ -10,19 +10,22 @@ namespace Colecoes
 
             arrayInteiros[0] = 10;
             arrayInteiros[1] = 20;
-            arrayInteiros[2] = 30;
+            arrayInteiros[2] = int.Parse("30"); //converte o valor dado de string para inteiro
+            
+            arrayInteiros[3] = 30; // acessando posição inexistente (3)
 
-            System.Console.WriteLine("Percorrendo o array pelo For");
-            for (int i = 0; i < arrayInteiros.Length; i++)
-            {
-                 System.Console.WriteLine(arrayInteiros[i]);
-            }
 
-            System.Console.WriteLine("Percorrendo o array pelo ForEach");
-            foreach (int item in arrayInteiros)
-            {
-                System.Console.WriteLine(item);
-            }
+            // System.Console.WriteLine("Percorrendo o array pelo For");
+            // for (int i = 0; i < arrayInteiros.Length; i++)
+            // {
+            //      System.Console.WriteLine(arrayInteiros[i]);
+            // }
+
+            // System.Console.WriteLine("Percorrendo o array pelo ForEach");
+            // foreach (int item in arrayInteiros)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
 
         }
     }
